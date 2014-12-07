e029a5236ad3aa1da5557e2e13974850274ce265
bl82ki <fred.bakker@ing.nl>
2014-12-07 11:49:40 +0100

    this is the message

--- hopje.rb
+++ hopje.rb
@@ -1,26 +1,2 @@
-class Sequence
-	def initialize(startFrom = 0)
-		@counter = startFrom
-	end
-	
-	def counter
-		@counter
-	end
-	
-	def next
-		n = Sequence.new(@counter + 1)
-		n
-	end
-end
-
-class Alternate
-	def initialize(first, second)
-		@first = first
-		@second = second
-	end
-	
-	def next
-		puts @first
-		Alternate.new(@second, @first)
-	end
+class Hydiho
 end
